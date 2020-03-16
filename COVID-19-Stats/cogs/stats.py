7e9a61bfb2ba13448181be932b3f0dfd5f3ad43d
Zorkai <me@zorkai.tech>
2020-03-16 10:03:48 +0100

    Update stats.py

--- COVID-19-Stats/cogs/stats.py
+++ COVID-19-Stats/cogs/stats.py
@@ -19,6 +19,7 @@ class Stats(commands.Cog):
     @commands.command()
     async def stats(self, ctx, *, country):
         "See COVID-19 Stats about a country."
+        country = country.title()
         querystring = {"country": country}
         async with aiohttp.ClientSession() as cs:
             async with cs.get(
