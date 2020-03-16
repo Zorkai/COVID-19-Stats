21f961c7f5ff7b2665e7cde0fca1b9e97b69d8a6
Zorkai <me@zorkai.tech>
2020-03-16 15:39:27 +0100

    Update help.py
    little fixes

--- COVID-19-Stats/cogs/help.py
+++ COVID-19-Stats/cogs/help.py
@@ -22,7 +22,7 @@ class HelpCommand(commands.HelpCommand):
             cmds = await self.filter_commands(cmds)
             if not cog or not cmds:
                 continue
-            content = "\n".join(f"{prefix}{cmd}" if cmd.signature is ""
+            content = "\n".join(f"{prefix}{cmd}" if cmd.signature == ""
                                 else f"{prefix}{cmd} {cmd.signature}"
                                 for cmd in cmds)
             embed.add_field(
@@ -38,7 +38,7 @@ class HelpCommand(commands.HelpCommand):
             color=bot.EMBED_COLOR,
             timestamp=datetime.utcnow())
         ls = await self.filter_commands(cog.get_commands(), sort=True)
-        content = "\n".join(f"{prefix}{cmd}" if cmd.signature is "" else
+        content = "\n".join(f"{prefix}{cmd}" if cmd.signature == "" else
                             f"{prefix}{cmd} {cmd.signature}" for cmd in ls)
         embed.add_field(name="Commands", value=f"```\n{content}```")
         embed.set_footer(text=bot.BOT_NAME, icon_url=bot.user.avatar_url)
@@ -51,7 +51,7 @@ class HelpCommand(commands.HelpCommand):
             title=f"**Infos about {command.name}**",
             color=bot.EMBED_COLOR,
             timestamp=datetime.utcnow())
-        content = f"{prefix}{command}" if command.signature is "" else f"{prefix}{command} {command.signature}"
+        content = f"{prefix}{command}" if command.signature == "" else f"{prefix}{command} {command.signature}"
         embed.add_field(name="Usage", value=f"```\n{content}```", inline=False)
         if command.help is not None:
             embed.add_field(
